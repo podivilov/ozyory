06976ba93dbb5810e2e512ecf47da7a238059c3f
Mihail Podivilov <mihail@podivilov.ru>
2020-06-17 14:08:44 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# ozyory
 «Ozyory» Aegea theme
---
+++ theme-info.php
@@ -0,0 +1,21 @@
+<?php return array (
+
+  'index' => 13,
+
+  'display_name' => array (
+    'en' => 'Ozyory',
+    'ru' => 'Озёры',
+    'uk' => 'Озери',
+  ),
+
+  'colors' => array (
+    'background' => '#fff',
+    'headings' => '#000',
+    'text' => '#000',
+    'link' => '#ff5020',
+  ),
+
+  'meta_viewport' => 'width=device-width, initial-scale=1',
+  'supports_dark_mode' => true,
+
+); ?>
